b38401d93b7ba91da41b019f1a734db3903a8f4f
ericdong <dongjiaquan89@gmail.com>
2026-08-22 22:57:30 +0800

    clean useless test files

--- tests/dogfood/fixtures/scratch-buggy/target/debug/deps/api-160453b58b69cd60.d
+++
@@ -1,5 +0,0 @@
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/api-160453b58b69cd60.d: tests/api.rs
-
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/api-160453b58b69cd60: tests/api.rs
-
-tests/api.rs:
--- tests/dogfood/fixtures/scratch-buggy/target/debug/deps/scratch_lib-3bf31444c3f16062.d
+++
@@ -1,9 +0,0 @@
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/scratch_lib-3bf31444c3f16062.d: src/lib.rs src/math.rs src/strings.rs
-
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/libscratch_lib-3bf31444c3f16062.rlib: src/lib.rs src/math.rs src/strings.rs
-
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/libscratch_lib-3bf31444c3f16062.rmeta: src/lib.rs src/math.rs src/strings.rs
-
-src/lib.rs:
-src/math.rs:
-src/strings.rs:
