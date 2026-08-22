f9efe16b07885fe899e559c38c1b0d34575d087a
ericdong <dongjiaquan89@gmail.com>
2026-08-22 22:57:30 +0800

    clean useless test files

--- tests/dogfood/fixtures/scratch-buggy/target/debug/deps/scratch_lib-a208b17713842423.d
+++
@@ -1,7 +0,0 @@
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/scratch_lib-a208b17713842423.d: src/lib.rs src/math.rs src/strings.rs
-
-/home/ed/workspace/app/work/shannon/shannon-mono/tests/dogfood/fixtures/scratch-buggy/target/debug/deps/scratch_lib-a208b17713842423: src/lib.rs src/math.rs src/strings.rs
-
-src/lib.rs:
-src/math.rs:
-src/strings.rs:
